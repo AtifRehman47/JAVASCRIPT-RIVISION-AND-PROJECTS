34fb4c3a1ac5f9e1342ae3f40d08d075a9bb8e3f
AtifRehman47 <atifrehman401@gmail.com>
2026-01-14 00:00:47 +0500

    variable and data type
    var and datatypes

---
+++ Topic1-Variable & Datatypes/app.js
@@ -0,0 +1,75 @@
+// var myname = 'Atif'
+// console.log(myname);
+
+// let myname = 'Atif';
+// let myAge = 20;
+// let myQualification = 'BBIT'
+
+// console.log(myname,myAge,myQualification);
+
+
+// const mybook = 'learnjs';
+// const live = 'karachi';
+// const country = 'Pakistan';
+
+// console.log(mybook, live,country);
+
+
+// const value checking 
+
+// const learn = 'javascript';
+// const = learn = 'React';
+
+
+// Primitive and non primitive types 
+
+// primitive 
+
+// let a = 10;
+// let b = a;
+
+// a = 20;
+
+// console.log(a);
+// console.log(b);
+
+
+
+// non primitive 
+
+
+// let obj1 = {
+//         myName : 'Atif',
+//         Age : 20,                      
+// }
+// let obj2 = obj1;
+
+// obj1.myName = 'AtifRehman';
+
+// console.log(obj1.myName);
+// console.log(obj1.myName);
+
+
+// Again primitive and nonprimitive 
+
+// let a = 20;
+// let b = a;
+
+// a = 30;
+
+// console.log(a);
+// console.log(b);
+
+
+// nonprimitive 
+
+// let obj1 = {
+//        myName:'Atif',
+//        myAge : 20,                       
+// }
+
+// let obj2 = obj1
+// obj1.myName = 'AtifRehman'
+// console.log(obj1.myName);
+// console.log(obj2.myName);
+
